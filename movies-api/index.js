2aaf5b5c6ea9775573d3cc12790e4b2c66114c7f
Daijie Liu <20087611@mail.wit.ie>
2022-08-23 21:04:27 +0100

    Mongo lab Included Mongoose

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import './db';
+import './seedData'
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
 
